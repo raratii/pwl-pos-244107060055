8ca8cf7d1328cf57b6d6c873b37ca955a3cf1328
raratii <purnamaratih419@gmail.com>
2026-09-03 15:20:36 +0700

    feat: implement TransactionController with basic CRUD methods and routing

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,8 +1,22 @@
 <?php
 
 namespace App\Http\Controllers;
-
-abstract class Controller
+class TransactionController extends Controller
 {
-    //
+    public function create()
+    {
+        return 'Halaman kasir (belum ada tampilan)';
+    }
+    public function store()
+    {
+        return 'Transaksi disimpan (belum ada logika penyimpanan)';
+    }
+    public function index()
+    {
+        return 'Daftar transaksi';
+    }
+    public function show(string $id)
+    {
+        return "Detail transaksi #{$id}";
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/halo', function () {
+    return ('Halo dari simple POS');
+});
+Route::get('/pos', [TransactionController::class, 'create']) ->name('pos.create');
+Route::post('/pos', [TransactionController::class, 'store'])
+    ->name('pos.store');
+Route::get('/transactions', [TransactionController::class, 'index']);
